b02b79d8c302a3160e9b6733e880d16e2d19698b
code-250 <richmunye@gmail.com>
2021-07-19 13:16:57 +0200

    start grabing form inputs using vuejs

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,12 @@
 /* eslint-disable key-spacing */
 <template>
-  <h1>{{title}}</h1>
-  <p>Welcome....</p>
-  <div v-if="showModal">
-  <Modal theme="saled" @close="toggleModal">
-    <template v-slot:links>
-      <a href="#">signup</a>
-      <a href="#">get more info</a>
-    </template>
-    <h1>Becoming a Ninja</h1>
-    <p>there is a way that any one can become a ninja</p>
-  </Modal>
-  </div>
-  <teleport to=".modals" v-if="showModal2">
-    <Modal @close="toggleModal2">
-     <h1>hello world</h1>
-     <p>please make it work</p>
-    </Modal>
-  </teleport>
-  <button @click="toggleModal">open modal</button>
-  <button @click="toggleModal2">open modal 2</button>
+  <h1>Forms & inputs</h1>
 </template>
 
 <script>
-import Modal from './components/Modal.vue';
 
 export default {
-  components: {
-    Modal,
-  },
-  data() {
-    return {
-      title: ' My first Vue App :) ',
-      header: 'sign up for some videos',
-      text: 'there are more you can see',
-      showModal: false,
-      showModal2: false,
-    };
-  },
-  methods: {
-    handleClick() {
-      this.$refs.name.classList.add('active');
-      this.$refs.name.focus();
-    },
-    toggleModal() {
-      this.showModal = !this.showModal;
-    },
-    toggleModal2() {
-      this.showModal2 = !this.showModal2;
-    },
-  },
+  components: {},
 };
 </script>
 
